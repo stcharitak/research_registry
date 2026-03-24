6f4b195b815d6bb9ad298e077b4ea11d27cae0ee
Stavros Charitakis <st.charitak@gmail.com>
2026-03-24 12:15:35 +0100

    add serializers for models

---
+++ backend/participants/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Participant
+
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = "__all__"
---
+++ backend/studies/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Study
+
+
+class StudySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Study
+        fields = "__all__"
