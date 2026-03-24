bc153ea4c2eec7f60279f45858949b5433a0a7b7
Stavros Charitakis <st.charitak@gmail.com>
2026-03-24 14:00:46 +0100

    add application api

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Application
+from .serializers import ApplicationSerializer
+
+
+class ApplicationViewSet(ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
--- backend/research_registry/urls.py
+++ backend/research_registry/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("studies.urls")),
     path("api/", include("participants.urls")),
+    path("api/", include("applications.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
