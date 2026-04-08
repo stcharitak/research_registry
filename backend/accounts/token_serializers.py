c04f778becc6cc802325356b37dbf34dabe67f13
Stavros Charitakis <st.charitak@gmail.com>
2026-04-08 13:16:41 +0200

    add user attributes to the jwt token

--- backend/accounts/token_serializers.py
+++ backend/accounts/token_serializers.py
@@ -7,5 +7,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         token["username"] = user.username
+        token["first_name"] = user.first_name if user.first_name else None
+        token["last_name"] = user.last_name if user.last_name else None
+        token["email"] = user.email if user.email else None
         token["role"] = user.role.name if user.role else None
         return token
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,11 +1,10 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .token_serializers import CustomTokenObtainPairSerializer
 
 from .serializers import MeSerializer
+from .token_serializers import CustomTokenObtainPairSerializer
 
 
 class MeView(APIView):
